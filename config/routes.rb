a63a4c713a1f69302f25bb9d445b800370ec2a51
marykellar <marymac@maryxmac.com>
2020-04-07 15:56:28 -0400

    added backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  
-
   resources :resources
   resources :categories
+  get '/categories/:category_id/resources/:id', to: 'resources#add_category'
+  get '/categories/', to: 'categories#index'
+  get '/categories/:id', to: 'categories#show'
+
   # ============== Auth routes =====================
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ groups = Category.create!(name:"Group Work")
 
 p "#{Category.count} categories were created"
 
-Resource.create!(name: 'Creating Zoom breakout rooms', categories: [env, groups, check], link:"https://support.zoom.us/hc/en-us/articles/360032752671-Pre-assigning-participants-to-breakout-rooms#h_36f71353-4190-48a2-b999-ca129861c1f4", decription:"Use zoom breakout rooms to form het/homogenous groupings.  Check in with your groups by stopping into breakout rooms.",user_id:2)
+Resource.create!(name: 'Creating Zoom breakout rooms', categories: [env, groups, check], link:"https://support.zoom.us/hc/en-us/articles/360032752671-Pre-assigning-participants-to-breakout-rooms#h_36f71353-4190-48a2-b999-ca129861c1f4", description:"Use zoom breakout rooms to form het/homogenous groupings.  Check in with your groups by stopping into breakout rooms.",user_id:2)
 Resource.create!(name: 'Using Google Classroom Forms for Quizzes', categories: [check, formative, summative], link:"https://youtu.be/KlV01IHAovw",description:"Add a google form to your drive and use it as a quiz.",user_id:1)
 Resource.create!(name: 'Setting up your Zoom classroom', categories: [env], link:"https://youtu.be/ocTFLh46fgE",description:"Go through all the settings in zoom with the pros and cons of each in less than 23 minutes.", user_id:1)
 
